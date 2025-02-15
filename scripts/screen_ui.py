4a6a31811aae90a549644b6d1d2fc0e5b18c68f3
Bruke Wossenseged <brukew@mit.edu>
2025-02-14 23:54:02 -0500

    new images + show position

--- scripts/screen_ui.py
+++ scripts/screen_ui.py
@@ -5,8 +5,7 @@ from PIL import Image, ImageTk, ImageOps
 import random
 import itertools
 
-POS = 0
-DIST = 1
+POS = 1
 
 WEIGHTS = {}
 
@@ -30,9 +29,9 @@ class MarioKartUI:
         self.load_images()
         
         ttk.Label(self.frm, image=self.kai_tk).grid(column=16, row=2)
-        ttk.Label(self.frm, image=self.toad_tk).grid(column=2, row=2)
         
         ttk.Button(self.frm, text="Quit", command=root.destroy).grid(column=1, row=1)
+        ttk.Label(self.frm, text=f"Position: {POS}", font=("Helvetica", 28)).grid(column=4, row=1)
         ttk.Button(self.frm, text="Item Box Checkpoint", command=self.animate_item_selection).grid(column=1, row=2)
         
         self.item_label = ttk.Label(self.frm, image=self.item_images[0])
@@ -46,8 +45,7 @@ class MarioKartUI:
             new_img.paste(img, ((size[0] - img.size[0]) // 2, (size[1] - img.size[1]) // 2), img)  # Center image
             return ImageTk.PhotoImage(new_img)
         
-        self.toad_tk = load_image(self.characters_path + "toad_kart.webp", (200, 200))
-        self.kai_tk = load_image(self.characters_path + "kai.webp", (200, 200))
+        self.kai_tk = load_image(self.characters_path + "kai_toad_avi.png", (200, 200))
         
         item_names = ["redshell", "bobomb", "blueshell", "shock", "mushroom", "goldmushroom", "bill", "banana"]
         self.item_images = [load_image(self.items_path + f"{name}.webp", (200, 200)) for name in item_names]
@@ -58,7 +56,7 @@ class MarioKartUI:
                 self.item_label.configure(image=self.item_images[index % len(self.item_images)])
                 self.root.after(100, update_image, index + 1)
             else:
-                self.item_label.configure(image=random.choice(self.item_images))  # Final selection, add weights=weights(POS, DIST)
+                self.item_label.configure(image=random.choice(self.item_images))  # Final selection, add weights=WEIGHTS[POS]
         
         self.root.after(0, update_image)
 
