4ae863b2bd54a36ca3474b326f270c6797671ce4
Bruke Wossenseged <brukew@mit.edu>
2025-02-16 22:26:55 -0500

    updated utils

--- src/scripts/server/utils.py
+++ src/scripts/server/utils.py
@@ -1,13 +1,5 @@
 from collections import defaultdict
 import globals
 
-def convert_coordinate_to_index(x: int, y: int) -> int:
-    return y #Placeholder for testing
-
-def select_item_for_kart(rank: int) -> str:
-    return
-
 def update_rankings():
-    new_rank = list(globals.kart_data.keys())
-    new_rank.sort(key = lambda x: (kart_data[x]["laps"], kart_data[x]["index"]), reverse = True)
-    globals.kart_positions = new_rank
+    globals.kart_positions = sorted(globals.kart_data.keys(), key=lambda k: (globals.kart_data[k]["laps"], globals.kart_data[k]["index"]), reverse=True)
